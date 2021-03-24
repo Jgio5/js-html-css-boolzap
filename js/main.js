9afecf7c7e050a7306d61190b2152511343afd38
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-03-24 23:37:02 +0100

    Milestone 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var app = new Vue( {
     el: '#app',
     data: {
+        user: {
+            name: 'Giorgio Lopez',
+            avatar: '_2'
+        },
         contacts: [
         {
             name: 'Michele',
@@ -85,12 +89,18 @@ var app = new Vue( {
                 }
             ],
         },
-        ]
+        ],
+    //parametro per selezionare l'utente e la chat da visualizzare
+    activeChat : '-1'
     }
 });
 
 Vue.config.devtools = true
 
 // Milestone 1
-// ●	Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// ●	Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+// Milestone 2
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// Click sul contatto mostra la conversazione del contatto cliccato
