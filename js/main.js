aa0cc77b4364a13eadbacfdceb9499de716288b8
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-07-12 16:41:13 +0200

    Msg delate - css

--- js/main.js
+++ js/main.js
@@ -1,182 +1,215 @@
-var app = new Vue( {
-    el: '#app',
+var app = new Vue({
+    el: '#myApp',
     data: {
         user: {
             name: 'Giorgio Lopez',
-            avatar: '_2'
+            avatar: '-profilo'
         },
-        //parametro per selezionare l'utente e la chat da visualizzare
-        activeChat : '-1',
-        //nuova chat
+        activeChat: '-1',
+        activeBoxSettings:'-1',
         newMsg: '',
-        //ricerca del nome
-        filterChatName: '',
-        //emoticons
+        filterChat: '',
         emoticon: ':)',
+        
         contacts: [
-        {
-            name: 'Michele',
-            avatar: '_1',
-            visible: true,
-            messages: [
-                {
-                    date: '10/01/2020 15:30:55',
-                    text: 'Hai portato a spasso il cane?',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    text: 'Ricordati di dargli da mangiare',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 16:15:22',
-                    text: 'Tutto fatto!',
-                    status: 'received'
-                }
-            ],
-        },
-        {
-            name: 'Fabio',
-            avatar: '_2',
-            visible: true,
-            messages: [
-                {
-                    date: '20/03/2020 16:30:00',
-                    text: 'Ciao come stai?',
-                    status: 'sent'
-                },
-                {
-                    date: '20/03/2020 16:30:55',
-                    text: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
-                },
-                {
-                    date: '20/03/2020 16:35:00',
-                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'sent'
-                }
-            ],
-        },
-        {
-            name: 'Samuele',
-            avatar: '_3',
-            visible: true,
-            messages: [
-                {
-                    date: '28/03/2020 10:10:40',
-                    text: 'La Marianna va in campagna',
-                    status: 'received'
-                },
-                {
-                    date: '28/03/2020 10:20:10',
-                    text: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
-                },
-                {
-                    date: '28/03/2020 16:15:22',
-                    text: 'Ah scusa!',
-                    status: 'received'
-                }
-            ],
-        },
-        {
-            name: 'Luisa',
-            avatar: '_4',
-            visible: true,
-            messages: [
-                {
-                    date: '10/01/2020 15:30:55',
-                    text: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    text: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
-                }
-            ],
-        },
-        ],
+            {
+                name: 'Andrea',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Andrei',
+                avatar: '_2',
+                visible: true,
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                    },
+                    {
+                        date: '12/07/2021 16:35:00',
+                        text: 'Aperitivo stasera?',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Cristian',
+                avatar: '_3',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    },
+                    {
+                        date: '12 07 2021 04:25:05',
+                        text: 'stasera cinema?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '12 07 2021 04:25:07',
+                        text: 'Certo, chiamiamo anche gli altri?',
+                        status: 'received'
+                    },
+                    {
+                        date: '12 07 2021 04:25:14',
+                        text: 'si!',
+                        status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Laura',
+                avatar: '_4',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Lidia',
+                avatar: '_5',
+                visible: true,
+                messages: [
+                    {
+                        date: '12/07/2021 15:30:55',
+                        text: 'Ciao Giorgio, stasera cinema?',
+                        status: 'received'
+                    },
+                    {
+                        date: '12/07/2021 15:50:00',
+                        text: 'Ciao Michela, si! Chiamo anche gli altri?',
+                        status: 'send'
+                    },
+                    {
+                        date: '12/07/2021 15:30:55',
+                        text: 'Si!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Elvira',
+                avatar: '_6',
+                visible: true,
+                messages: [
+                    {
+                        date: '12/07/2021 15:30:55',
+                        text: 'Ma come hai fatto a scrivere in grassetto?',
+                        status: 'received'
+                    },
+                    {
+                        date: '12/07/2021 15:50:00',
+                        text: '*Visualizzato*',
+                        status: 'send'
+                    }
+                ],
+            },
+        ]
+
     },
-    methods : {
-        sendNewMessage() {
-            if(this.newMsg != '' && this.activeChat != -1)
-            var msgSend = new Object;
-            msgSend.date = this.myDate() + " " + this.myTime();
-            msgSend.text = this.newMsg;
-            msgSend.status = 'sent';
-            this.contacts[this.activeChat].messages.push(msgSend)
+    methods: {
+        sendMsg() {
+            if (this.newMsg != '' && this.activeChat != -1)
+            var msgSent = new Object;
+            msgSent.date = dayjs().format('DD MM YYYY hh:mm:ss');
+            msgSent.text = this.newMsg;
+            msgSent.status = 'sent';
+            this.contacts[this.activeChat].messages.push(msgSent)
             this.newMsg = '';
-            setTimeout(this.autoAnswer, 500);
+            setTimeout(this.autoAnswer, 1500);
         },
+
         autoAnswer() {
             var msgReceived = new Object;
-            msgReceived.date = this.myDate() + " " + this.myTime();
-            msgReceived.text = 'Ok';
+            msgReceived.date = dayjs().format('DD MM YYYY hh:mm:ss');
+            msgReceived.text = 'Va bene, sarà per la prossima!';
             msgReceived.status = 'received';
             this.contacts[this.activeChat].messages.push(msgReceived)
         },
+        //smile
         sendSmile() {
             if(this.activeChat != -1)
             var smile = new Object;
-            smile.date = this.myDate() + " " + this.myTime();
+            smile.date = dayjs().format('DD MM YYYY hh:mm:ss');
             smile.text = this.emoticon;
             smile.status = 'sent';
             this.contacts[this.activeChat].messages.push(smile);
-            setTimeout(this.autoSmile, 500);
+            setTimeout(this.autoSmile, 800);
         },
         autoSmile() {
             var msgReceived = new Object;
-            msgReceived.date = this.myDate() + " " + this.myTime();
+            msgReceived.date = dayjs().format('DD MM YYYY hh:mm:ss');
             msgReceived.text = ':P';
             msgReceived.status = 'received';
             this.contacts[this.activeChat].messages.push(msgReceived)
         },
-        myTime() {
-            var data = new Date();
-            var Hh, Mm, Ss;
-            Hh = data.getHours() + ":";
-            Mm = data.getMinutes() + ":";
-            Ss = data.getSeconds();
-            return Hh + Mm + Ss; 
-        },
-        myDate() {
-            var data = new Date();
-            var gg, mm, aaaa;
-            gg = data.getDate() + "/";
-            mm = data.getMonth() + 1 + "/";
-            aaaa = data.getFullYear();
-            return gg + mm + aaaa;
-        },
-        filterName() {
-            let filterChatName = this.filterChatName.toUpperCase();
+        filterSearch() {
+            let filterChat = this.filterChat.toUpperCase();
             this.contacts.filter(function (contact) {
-                if (contact.name.toUpperCase().includes(filterChatName)) {
+                if (contact.name.toUpperCase().includes(filterChat)) {
                     contact.visible = true
                 } else {
                     contact.visible = false
                 }
             });
+        },
+        
+        showSettingBox(index){
+            this.activeBoxSettings = index;
+        },
+
+        deleteMsg(index){
+            this.contacts[this.activeChat].messages.splice(index,1)
         }
-    }
-});
-
-Vue.config.devtools = true
-
-// Milestone 1
-// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
-
-// Milestone 2
-// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-// Click sul contatto mostra la conversazione del contatto cliccato
-
-// Milestone 3
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
-//far comparire anche sotto il nome utente in alto, la scritta (Tizio sta scrivendo...) per poi scomparire non appena arriva il messaggi ricevuto
-
-// Milestone4
-// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite
-// (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+    },
+})
